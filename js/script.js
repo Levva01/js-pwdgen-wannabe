07b899f81add496a5a6f65a95b754adc4eaa1fc4
Levva01 <vellanifederico13@gmail.com>
2022-04-27 14:04:12 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let name = prompt("Inserisci il tuo nome ");
+let lastName = prompt("Inserisci il tuo cognome: ");
+let favColor = prompt("Inserisci il tuo colore preferito: ");
+
+document.writeln(`${name}${lastName}${favColor}21`);
